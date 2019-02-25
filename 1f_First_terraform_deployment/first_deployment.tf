cd7601e0dbc6329be7f070f74c5aea96248b4a91
sanmanivannan <sanmanivannan@gmail.com>
2019-02-25 14:08:38 -0600

    removing the assess and sec key

--- 1f_First_terraform_deployment/first_deployment.tf
+++ 1f_First_terraform_deployment/first_deployment.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIAIXH2HCOZLVBNRLRA"
-  secret_key = "XZg1IOmVZBoWVl+mUjNZYeTdFRsqIFiyR8UJ0SUM"
+  access_key = "NRLRA"
+  secret_key = "eTdFRsqIFiyR8UJ0SUM"
   region     = "us-east-1"
 }
 resource "aws_instance" "example" {
